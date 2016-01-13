1bf7121aa17f7c7ce6e0e47b8bcd4e1ab768d3ee
OysterKing <comhghalldavid@live.com>
2016-01-13 14:26:59 +0000

    Created new class NetInitialiser to initialise the custom topology read by NetReader and launch mininet.

---
+++ NetInitialiser.py
@@ -0,0 +1,27 @@
+from mininet.clean import cleanup
+from mininet.cli import CLI
+from mininet.term import cleanUpScreens
+from mininet.net import Mininet
+from mininet.link import TCLink
+from mininet.util import dumpNodeConnections
+from mininet.log import setLogLevel
+from mininet.node import Controller, OVSController
+from NetReader import customTopo
+from bridge_switch import BridgeSwitch
+import sys
+#from utils import makeTerm, makeTerms
+from subprocess import Popen
+
+def customNet():
+	link_opts = {"bw":1000, "delay":0, "loss": 0, "use_htb": False}
+	filename = "netanim_topo.xml"
+	topo = customTopo(link_opts = link_opts, filename = filename)
+
+	net = Mininet(topo, switch = BridgeSwitch, controller = OVSController, link = TCLink)
+	net.start()
+
+def main():
+	customNet()
+
+if __name__ == '__main__':
+	main()
--- NetReader.py
+++ NetReader.py
@@ -7,11 +7,13 @@ try:
 
 	class customTopo(Topo):
 
-		def __init__(self, blocking = False, link_opts = {}, **opts, xmlFilename):
+		def __init__(self, blocking = False, link_opts = {}, xmlFilename = "netanim_topo.xml", **opts):
 
 			Topo.__init__(self, **opts)
 			hostIDs = []
 			switchIDs = []
+			hostCount = 0
+			switchCount = 0
 
 			with open(xmlFilename, 'r') as file:
 				data = file.readlines()
@@ -20,45 +22,48 @@ try:
 				if data[i].startswith("<nu") and data[i].count('r="255"'):
 					#host
 					hostCount += 1
-					name = "h." + str(hostCount)
+					name = "h" + str(hostCount)
 					host = self.addHost(name)
 					hostIDs.append(int(data[i - 1][10]))
 
 				elif data[i].startswith("<nu") and data[i].count('g="255"'):
 					#switch
 					switchCount += 1
-					name = "s." + str(switchCount)
+					name = "s" + str(switchCount)
 					switch = self.addSwitch(name, blocking = blocking)
 					switchIDs.append(int(data[i - 1][10]))
 
 			hostCount = 0
-			switchCOunt = 0
+			switchCount = 1 #hardcoded for testing
 
 			#Need to wait until all hosts and switches have been added before
-			# adding links.
+			# adding links. Need to find proper way of getting host and switch ids.
 			for i in range(len(data)):
 				if data[i].startswith("<link"):
 					#link
-					fromID = int(data[i][14]
-					toID = int(data[i][23]
+					fromID = int(data[i][14])
+					toID = int(data[i][23])
 					for j in range(len(hostIDs)):
-						if hostIDs[j] == fromID
+						if hostIDs[j] == fromID:
 							hostCount += 1
-							from = "h." + str(hostCount)
+							fromNode = str("h" + str(hostCount))
 
-						elif hostIDs[j] == toID
+						elif hostIDs[j] == toID:
 							hostCount += 1
-							to - "h." + str(hostCount)
+							toNode = str("h" + str(hostCount))
 
-						elif switchIDs[j] == fromID
-							switchCount += 1
-							from = "s." + str(switchCount)
+					for k in range(len(switchIDs)):
+						if switchIDs[k] == fromID:
+							#switchCount += 1
+							fromNode = str("s" + str(switchCount))
 
-						elif switchIDs[j] == toID
-							switchCount += 1
-							to = "s." + str(switchCount)
+						elif switchIDs[k] == toID:
+							#switchCount += 1
+							toNode = str("s" + str(switchCount))
 
-					self.addLink(from, to, **link_opts)
+					print fromNode
+					print toNode
+					self.addLink(fromNode, toNode, **link_opts)
 
 except ImportError:
 	pass
